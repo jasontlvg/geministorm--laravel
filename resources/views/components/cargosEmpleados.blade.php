9c0d3c0dc97abea91a37500e0b02b80302dbe287
jasontlvg <a1247528@uabc.edu.mx>
2020-03-23 22:44:41 -0700

    Cargos corregidos

--- resources/views/components/cargosEmpleados.blade.php
+++ resources/views/components/cargosEmpleados.blade.php
@@ -1,13 +1,22 @@
-<option>Biomedical Engineer</option>
-<option>Technical Specialist</option>
-<option>Farm Labor Contractor</option>
-<option>Janitorial Supervisor</option>
-<option>PR Manager</option>
-<option>Farm Labor Contractor</option>
-<option>Animal Care Workers</option>
-<option>Postmasters</option>
-<option>Fish Game Warden</option>
-
+<option>Gente de Calidad</option>
+<option>Gerente de Mantenimiento</option>
+<option>Gerente de Planta</option>
+<option>Gerente de Producción</option>
+<option>Responsable de Mejora Continua</option>
+<option>Responsable de Recursos Humanos</option>
+<option>Jefe de Mantenimiento</option>
+<option>Supervisor de Mantenimiento</option>
+<option>Jefe de turno</option>
+<option>Ingeniero de Producción</option>
+<option>Ingeniero de Manufactura</option>
+<option>Ingeniero de procesos</option>
+<option>Supervisor de Producción</option>
+<option>Analista de Procesos</option>
+<option>Asistente o Auxiliar</option>
+<option>Inspector de Calidad</option>
+<option>Técnico</option>
+<option>Operador de producción</option>
+<option>Otro</option>
 {{--<option>Supervisor</option>--}}
 {{--<option>Administrador</option>--}}
 {{--<option>Jefe de Grupo</option>--}}
